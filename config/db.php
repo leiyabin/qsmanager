c7762cd798e82ed293ad12e1e47fe406094297c3
leiyabin <957919360@qq.com>
2016-12-17 19:57:32 -0800

    ‘db’

---
+++ config/db.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    'class'       => 'yii\db\Connection',
+    'dsn'         => 'mysql:host=localhost;dbname=qs_web',
+    'username'    => 'qs_web_rw',
+    'password'    => '123456',
+    'charset'     => 'utf8',
+    'tablePrefix' => 't_'
+];
